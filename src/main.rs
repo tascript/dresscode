bc4343bc88fdf0cba7325165ccd27785b3f308b5
wataru-script <wataruscript8611@gmail.com>
2019-09-29 21:43:02 +0900

    get stdin

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use std::io::{self, Read};
 
 fn main() {
+    let stdin = get_stdin().unwrap();
+    println!("{:?}", stdin);
+}
+
+fn get_stdin() -> io::Result<String> {
     let mut buf = String::new();
-    io::stdin().read_to_string(&mut buf).unwrap();
-    println!("{:?}", buf);
+    io::stdin().read_to_string(&mut buf)?;
+    Ok(buf)
 }
