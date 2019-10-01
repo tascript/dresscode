f60bb29fa0c5ac631bfefe1a0fec43f131af69a0
wataru-script <wataruscript8611@gmail.com>
2019-10-01 21:55:37 +0900

    use arguments

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,23 @@ use std::io::{self, Read};
 use std::process::{Command, Stdio};
 
 fn main() {
+    let matches = App::new("dragontail")
+        .author("wataru-script")
+        .about("Dress up stdin")
+        .arg(
+            Arg::with_name("input")
+                .value_name("INPUT")
+                .help("stdin")
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("keyword")
+                .value_name("KEYWORD")
+                .help("Keyword")
+                .multiple(true)
+                .required(false),
+        )
+        .get_matches();
     let stdin = get_stdin().unwrap();
     println!("{}", stdin);
 }
